2e90fc6c6b26346c6ce891e90600b29c5a28fb7b
Kevin Formsma <kevin.formsma@gmail.com>
2021-12-07 17:20:06 -0500

    Solution for Day 6

---
+++ src/day6/mod.rs
@@ -0,0 +1,53 @@
+use std::{fs, fmt};
+
+#[derive(Copy, Clone, Hash, PartialEq, Eq)]
+struct Lanternfish {
+    colony: [u64; 9]
+}
+
+impl Lanternfish {
+    fn add_fish_from_string(&mut self, string: &str) {
+        self.colony[string.parse::<usize>().unwrap()] += 1;
+    }
+
+    fn tock(&mut self) {
+        // Get num new fish to be made
+        let new_gen_size = self.colony[0];
+
+        // Rotate the normal lifecycle 0..7 left once
+        self.colony[0..7].rotate_left(1);
+
+        // Move down the new spawn counts
+        self.colony[6] += self.colony[7];
+        self.colony[7] = self.colony[8];
+        self.colony[8] = new_gen_size;
+    }
+}
+
+impl fmt::Display for Lanternfish {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Lanternfish Colony ({})", self.colony[0])
+    }
+}
+
+pub fn execute_puzzle() {
+    let file_result = fs::read_to_string("src/day6/input.txt");
+
+    let puzzle_input = match file_result {
+        Ok(input) => input,
+        Err(error) => panic!("Unable to read puzzle input: {}", error),
+    };
+
+
+    let mut colony: Lanternfish = puzzle_input.split(',')
+        .fold(Lanternfish { colony: [0; 9] },|mut colony, number_str | {
+            colony.add_fish_from_string(number_str);
+            colony
+        });
+
+    for _day in 0..256 {
+        colony.tock();
+    }
+
+    println!("Day 6 Puzzle -- {}", colony.colony.iter().sum::<u64>());
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() {
     println!("Hello, Advent of Code 2021!");
@@ -16,4 +17,6 @@ fn main() {
     day4::execute_puzzle();
 
     day5::execute_puzzle();
+
+    day6::execute_puzzle();
 }
